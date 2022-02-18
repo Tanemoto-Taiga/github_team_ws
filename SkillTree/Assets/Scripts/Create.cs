71b37efdf607d4e303630a4b6036841bdd5a8c9f
ShogoSugimoto <c0b200817c@edu.teu.ac.jp>
2022-02-18 21:57:05 +0900

    Shogo

---
+++ SkillTree/Assets/Scripts/Create.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class Create : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject button1;
+    [SerializeField]
+    private GameObject canvas;
+ 
+    public void createButton()
+    {
+        GameObject button = Instantiate(button1, new Vector3(960.0f, 270.0f, 0.0f), Quaternion.identity);
+        button.transform.parent = canvas.transform;
+    }
+}
